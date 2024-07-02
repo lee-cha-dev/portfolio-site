feb40981adbf84ff1477e616172fda241aed6613
lee-cha-dev <130196023+lee-cha-dev@users.noreply.github.com>
2024-07-02 01:55:12 -0500

    Removed logic - lead to a blank page.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import theme from "./theme";
 import Projects from "./pages/Projects";
 
 function App() {
-    // This will be '/' in development and '/portfolio-site' in production if deployed to GitHub Pages
-    const basename = process.env.PUBLIC_URL;
-
-    return (
-        <ChakraProvider theme={theme}>
-            <BrowserRouter basename={basename}>
-                <div className="App">
-                    <div className="App-content">
-                        <Routes>
-                            <Route path="/" element={<Portfolio />} />
-                            <Route path="/projects" element={<Projects />} />
-                        </Routes>
-                    </div>
-                </div>
-            </BrowserRouter>
-        </ChakraProvider>
-    );
+  return (
+      <ChakraProvider theme={theme}>
+          <BrowserRouter>
+              <div className="App">
+                  <div className="App-content">
+                      <Routes>
+                          <Route path="/" element={<Portfolio />} />
+                          <Route path="/projects" element={<Projects />} />
+                      </Routes>
+                  </div>
+              </div>
+          </BrowserRouter>
+      </ChakraProvider>
+  );
 }
 
 export default App;
